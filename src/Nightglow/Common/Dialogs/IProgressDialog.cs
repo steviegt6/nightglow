a9486dec3bd51a8783453c951477ef43f6c66974
ppeb <ppeb@ppeb.me>
2023-08-05 02:56:46 -0500

    Interface formatting

--- src/Nightglow/Common/Dialogs/IProgressDialog.cs
+++ src/Nightglow/Common/Dialogs/IProgressDialog.cs
@@ -1,19 +1,28 @@
-using System;
+﻿using System;
 
 namespace Nightglow.Common.Dialogs;
 
 public interface IProgressDialog : IDisposable {
-    public void Initialize(string title, string header, string text, params DialogOption<IProgressDialog>[] opts);
-    public void SetFraction(double fraction);
-    public void SetHeader(string header);
-    public string GetHeader();
-    public void SetText(string text);
-    public string GetText();
-    public void Pulse();
-    public void SetPulseStep(double fraction);
+    void Initialize(string title, string header, string text, params DialogOption<IProgressDialog>[] opts);
+
+    void SetFraction(double fraction);
+
+    void SetHeader(string header);
+
+    string GetHeader();
+
+    void SetText(string text);
+
+    string GetText();
+
+    void Pulse();
+
+    void SetPulseStep(double fraction);
+
     /// <summary>
     ///     Pulses the progress bar every (<paramref name="ms"/>) while (<paramref name="condition"/>) returns true
     /// </summary>
-    public void PulseWhile(int ms, Func<bool> condition);
-    public void Close();
+    void PulseWhile(int ms, Func<bool> condition);
+
+    void Close();
 }
--- src/Nightglow/Common/Instances/ICreateInstance.cs
+++ src/Nightglow/Common/Instances/ICreateInstance.cs
@@ -1,10 +1,11 @@
-using System.Runtime.Versioning;
+﻿using System.Runtime.Versioning;
 using System.Threading.Tasks;
 
 namespace Nightglow.Common.Instances;
 
 [RequiresPreviewFeatures]
 public interface ICreateInstance {
-    public static abstract string DefaultIcon { get; }
-    public static abstract Task<Instance> Create(string name, string? icon);
+    static abstract string DefaultIcon { get; }
+
+    static abstract Task<Instance> Create(string name, string? icon);
 }
